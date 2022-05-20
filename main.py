6ca131efa75b86043b0feaffc7ce91020bc7b696
iTango21 <itango21@gmail.com>
2022-05-20 09:14:52 +0300

    Initial...

---
+++ main.py
@@ -0,0 +1,26 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+# from selenium.Java
+import time
+from random import randrange
+from fake_useragent import UserAgent
+
+# from random import randrange
+ua = UserAgent()
+ua = ua.random
+
+import requests
+from bs4 import BeautifulSoup
+import lxml
+import json
+import time
+
+# import xlsxwriter
