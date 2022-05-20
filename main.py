33c280a2c548d415c46fdc376527bfd29238de3f
iTango21 <itango21@gmail.com>
2022-05-20 09:47:20 +0300

    start...

--- main.py
+++ main.py
@@ -30,28 +30,85 @@ url = 'https://reinersuite.nrha.com/#/login'
 
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "User-Agent": f'{ua}'  # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
-    # like Gecko) Chrome/96.0.4664.45 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
 }
+
+with open('config.json', 'r', encoding='utf-8') as set_:
+    set_data = json.load(set_)
+
+set_email = set_data['set_email']
+set_pass = set_data['set_pass']
 
 print('start...')
 
 # 1
 #
-options = webdriver.FirefoxOptions()
-options.set_preference("general.useragent.override", f"{ua}")
+# options = webdriver.FirefoxOptions()
+# options.set_preference("general.useragent.override", f"{ua}")
+# options.set_preference("dom.webnotifications.enabled", False);
+#
+# s = Service('geckodriver.exe')
+#
+# driver = webdriver.Firefox(service=s, options=options)
 
-s = Service('geckodriver.exe')
 
-driver = webdriver.Firefox(service=s, options=options)
+# #HERE IT FINDS THE PATH
+# if getattr(sys, 'frozen', False):
+#     application_path = os.path.dirname(sys.executable)
+# else:
+#     try:
+#         app_full_path = os.path.realpath(__file__)
+#         application_path = os.path.dirname(app_full_path)
+#     except NameError:
+#         application_path = os.getcwd()
+#
+# #Here we create the variable that is going to be used to all the functions for the path
+# path = os.path.join(application_path)
 
-driver.implicitly_wait(1.5)
-driver.get(url)
+chrome_path = ("./chromedriver.exe")
+
+options = webdriver.ChromeOptions()
+options.headless = False
+options.add_argument("--incognito")
+options.add_argument("start-maximized")
+#
+options.add_argument('--disable-blink-features=AutomationControlled')
+#
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.add_experimental_option('useAutomationExtension', False)
+browser = webdriver.Chrome(options=options, executable_path=chrome_path)
+
+
+
+browser.implicitly_wait(1.5)
+browser.get(url)
 
 time.sleep(5)
-source_html = driver.page_source
+source_html = browser.page_source
+
+
+checkbox_xp = '//*[@id="checkbox1"]'
+start_time = time.time()
+try:
+    WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH, checkbox_xp))).click()
+except:
+    pass
+finish_time = time.time() - start_time
+
+email_xp = '//*[@id="content"]/div/div/section/div[2]/form/div[1]/div[1]/input'
+in_email = browser.find_element(By.XPATH, email_xp)
+in_email.send_keys(set_email)
+
+pass_xp = '//*[@id="content"]/div/div/section/div[2]/form/div[1]/div[2]/input'
+in_pass = browser.find_element(By.XPATH, pass_xp)
+in_pass.send_keys(set_pass)
+
+
+
+
+time.sleep(0.9)
 
 
 time.sleep(5)
-driver.close()
-driver.quit()
+browser.close()
+browser.quit()
