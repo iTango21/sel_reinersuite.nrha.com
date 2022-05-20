2c4f39d1075325293b8c5fbcb07a74af535d1841
iTango21 <itango21@gmail.com>
2022-05-20 09:19:33 +0300

    start...

--- main.py
+++ main.py
@@ -24,3 +24,34 @@ import json
 import time
 
 # import xlsxwriter
+
+url = 'https://reinersuite.nrha.com/#/login'
+
+
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "User-Agent": f'{ua}'  # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
+    # like Gecko) Chrome/96.0.4664.45 Safari/537.36"
+}
+
+print('start...')
+
+# 1
+#
+options = webdriver.FirefoxOptions()
+options.set_preference("general.useragent.override", f"{ua}")
+
+s = Service('geckodriver.exe')
+
+driver = webdriver.Firefox(service=s, options=options)
+
+driver.implicitly_wait(1.5)
+driver.get(url)
+
+time.sleep(5)
+source_html = driver.page_source
+
+
+time.sleep(5)
+driver.close()
+driver.quit()
